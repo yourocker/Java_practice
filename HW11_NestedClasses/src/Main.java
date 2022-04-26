3ed5e55c70caf7ea5e8fb03748b9c74483898dc3
yourock <awlwlb@yzndex.ru>
2022-04-26 20:52:25 +0400

    add nested classes

---
+++ HW11_NestedClasses/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    Table table = new Table();
+        table.put("yourock", 34);
+        table.put("leisan", 30);
+        table.put("volodya", 69);
+        table.put("petya", 11);
+
+        Table.Iterator iterator1 = table.new Iterator();
+
+        while (iterator1.hasNext()) {
+            Table.TableEntry entry = iterator1.next();
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+    }
+}
---
+++ HW11_NestedClasses/src/Table.java
@@ -0,0 +1,57 @@
+public class Table {
+    // static nested class
+    public static class TableEntry {
+        private String key;
+        private int value;
+
+        public TableEntry(String key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public String getKey() {
+            return key;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
+    public class Iterator {
+        private int current = 0;
+        public TableEntry next() {
+            TableEntry entry = entries[current];
+            current++;
+            return entry;
+        }
+
+        public boolean hasNext() {
+            return current < count;
+        }
+    }
+
+    private static final int MAX_ENTRIES_COUNT = 10;
+
+    private TableEntry entries[];
+    private int count;
+
+    public Table() {
+        this.entries = new TableEntry[MAX_ENTRIES_COUNT];
+        this.count = 0;
+    }
+
+    public void put (String key, int value) {
+        entries[count] = new TableEntry(key, value);
+        count++;
+    }
+
+    public int get (String key) {
+        for (int i = 0; i < count; i++) {
+            if(entries[i].key.equals(key)) {
+                return entries[i].value;
+            }
+        }
+        return -1;
+    }
+}
