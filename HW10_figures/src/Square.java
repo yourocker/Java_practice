f536a64bd55fffea7746a79c46f4854270ad90b3
yourock <awlwlb@yzndex.ru>
2022-04-26 19:09:01 +0400

    HW10_figures

--- HW10_figures/src/Square.java
+++ HW10_figures/src/Square.java
@@ -1,2 +1,59 @@
-public class Square {
+import java.util.Scanner;
+
+public class Square implements Scale, Move {
+    private int centOfSqX;
+    private int centOfSqY;
+    private int lenOfSqSide;
+
+    public Square(int centOfSqX, int centOfSqY, int lenOfSqSide) {
+        this.centOfSqX = centOfSqX;
+        this.centOfSqY = centOfSqY;
+        this.lenOfSqSide = lenOfSqSide;
+    }
+
+    public void areaOfSq() {
+        if (lenOfSqSide > 0) {
+            int areaOfSqRes = lenOfSqSide * lenOfSqSide;
+            System.out.println("Площадь квадрата равна " + areaOfSqRes);
+        } else {
+            System.out.println("Площадь меньше или равна нулю!!!");
+        }
+    }
+
+    public void perOfSq() {
+        if (lenOfSqSide > 0) {
+            int perOfSqRes = lenOfSqSide * 4;
+            System.out.println("Периметр квадрата равен " + perOfSqRes);
+        } else {
+            System.out.println("Периметр меньше или равен нулю!!!");
+        }
+    }
+
+    public int getCentOfSqX() {
+        return centOfSqX;
+    }
+
+    public int getCentOfSqY() {
+        return centOfSqY;
+    }
+
+    public int getLenOfSqSide() {
+        return lenOfSqSide;
+    }
+
+    @Override
+    public void move() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введи новые координаты центра квадрата по оси X");
+        centOfSqX = scanner.nextInt();
+        System.out.println("Введи новые координаты центра квадрата по оси Y");
+        centOfSqY = scanner.nextInt();
+    }
+
+    @Override
+    public void changeScale() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введи масштаб квадрата");
+        lenOfSqSide *= scanner.nextInt();
+    }
 }
