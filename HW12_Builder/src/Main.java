02ef1cfb20fa4abae43116b96d9c298a07c8dfc8
yourock <awlwlb@yandex.ru>
2022-04-27 16:58:37 +0400

    fix builder

--- HW12_Builder/src/Main.java
+++ HW12_Builder/src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        //User Yuriy = new User("Yuriy", "Maryin", 34, 1.97, true);
         User Yuriy = User.builder()
                 .firstName("Yuriy")
                 .lastName("Maryin")
@@ -8,14 +7,12 @@ public class Main {
                 .height(1.97)
                 .isWorker(true)
                 .build();
-        //User Leisan = new User("Leisan", null, 0, 0.0, true);
         User Leisan = User.builder()
                 .firstName("Leisan")
                 .build();
-        //User Volodya = new User("Volodya", "Pupkin",0,0.0,false);
         User Volodya = User.builder()
                 .firstName("Volodya")
-                .lastName("Huylo")
+                .lastName("Pupkin")
                 .age(69)
                 .build();
     }
